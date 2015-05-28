68a921375f9e1ee83bd1853a4f9e6b08d0f0b672
Arachis <TheOldNutMan@gmail.com>
2015-05-27 19:27:55 -0700

    Added struct that represents a floor object in-game: X, Y, Z

---
+++ Sulakore/Habbo/HPoint.cs
@@ -0,0 +1,46 @@
+﻿/* Copyright
+
+    GitHub(Source): https://GitHub.com/ArachisH/Sulakore
+
+    .NET library for creating Habbo Hotel desktop applications.
+    Copyright (C) 2015 Arachis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    See License.txt in the project root for license information.
+*/
+
+namespace Sulakore.Habbo
+{
+    public struct HPoint
+    {
+        public int X { get; }
+        public int Y { get; }
+        public string Z { get; }
+
+        public HPoint(int x, int y)
+            : this(x, y, "0.0")
+        { }
+        public HPoint(int x, int y, string z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string ToString()
+            => $"{{X={X},Y={Y},Z={Z}}}";
+    }
+}
