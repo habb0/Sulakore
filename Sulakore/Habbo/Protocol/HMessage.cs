d88e57e67e2a14f355888ef035b9d4a9f3323852
Arachis <TheOldNutMan@gmail.com>
2015-06-01 10:23:59 -0700

    Made HMessage inherit from IHPacketBase. Removed some stuff... Renamed some stuff...

--- Sulakore/Habbo/Protocol/HMessage.cs
+++ Sulakore/Habbo/Protocol/HMessage.cs
@@ -2,7 +2,7 @@
 
     GitHub(Source): https://GitHub.com/ArachisH/Sulakore
 
-    .NET library for creating Habbo Hotel desktop applications.
+    .NET library for creating Habbo Hotel related desktop applications.
     Copyright (C) 2015 Arachis
 
     This program is free software; you can redistribute it and/or modify
@@ -25,35 +25,31 @@
 using System;
 using System.Text;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 using Sulakore.Habbo.Protocol.Encoders;
 
 namespace Sulakore.Habbo.Protocol
 {
-    public class HMessage : IHMessage
+    public class HMessage : HPacketBase
     {
         private byte[] _toBytesCache;
         private string _toStringCache;
         private bool _beganConstructing;
 
         private readonly List<byte> _body;
-        private readonly List<object> _read, _written;
 
         private static readonly object _splitLock;
 
         private ushort _header;
         /// <summary>
-        /// Gets or sets the header of the packet.
+        /// Gets or sets the header of this <see cref="HMessage"/>.
         /// </summary>
-        public ushort Header
+        public override ushort Header
         {
             get { return _header; }
             set
             {
-                if (IsCorrupted || _header == value)
-                    return;
-                else
+                if (!IsCorrupted || _header != value)
                 {
                     _header = value;
                     ResetCache();
@@ -62,35 +58,38 @@ namespace Sulakore.Habbo.Protocol
         }
 
         /// <summary>
-        /// Gets or sets the position that determines where to begin the next read/write operation of an object.
+        /// Gets or sets the position that determines where to begin the next read/write operation in this <see cref="HMessage"/>.
         /// </summary>
-        public int Position { get; set; }
+        public override int Position { get; set; }
         /// <summary>
-        /// Gets or sets a value that indicates the destination for the packet.
+        /// Gets or sets the <see cref="HDestination"/> for this <see cref="HMessage"/>.
         /// </summary>
-        public HDestination Destination { get; set; }
+        public override HDestination Destination { get; set; }
 
         /// <summary>
-        /// Gets a value that determines whether given block of data is readable/writable.
+        /// Gets a value that determines whether this <see cref="HMessage"/> is readable/writable.
         /// </summary>
-        public bool IsCorrupted { get; }
+        public override bool IsCorrupted { get; }
         /// <summary>
-        /// Gets the length of the packet, excluding the first four bytes(Length) of the original block of data.
+        /// Gets the length of this <see cref="HMessage"/>.
         /// </summary>
-        public int Length { get; private set; }
+        public override int Length { get; protected set; }
         /// <summary>
-        /// Gets the block of data considered as the body of the packet, excluding the first six bytes(Length, Header) of the original block of data.
+        /// Gets the block of data considered as the body of the packet that excludes the header.
         /// </summary>
-        public byte[] Body { get; private set; }
+        public override byte[] Body { get; protected set; }
 
+        private readonly List<object> _read;
         /// <summary>
-        /// Gets a read-only collection of objects that have been read from the packet so far.
+        /// Gets a <see cref="IReadOnlyList{T}"/> of type <see cref="object"/> containing read values from this <see cref="HMessage"/>.
         /// </summary>
-        public ReadOnlyCollection<object> ChunksRead { get; }
+        public IReadOnlyList<object> ValuesRead => _read;
+
+        private readonly List<object> _written;
         /// <summary>
-        /// Gets a read-only collection of objects that have been written to the packet so far.
+        /// Gets a <see cref="IReadOnlyList{T}"/> of type <see cref="object"/> containing the written values of this <see cref="HMessage"/>.
         /// </summary>
-        public ReadOnlyCollection<object> ChunksWritten { get; }
+        public IReadOnlyList<object> ValuesWritten => _written;
 
         static HMessage()
         {
@@ -101,10 +100,8 @@ namespace Sulakore.Habbo.Protocol
             _body = new List<byte>();
             _read = new List<object>();
             _written = new List<object>();
-
-            ChunksRead = new ReadOnlyCollection<object>(_read);
-            ChunksWritten = new ReadOnlyCollection<object>(_written);
         }
+
         public HMessage(byte[] data)
             : this(data, HDestination.Client)
         { }
@@ -128,7 +125,7 @@ namespace Sulakore.Habbo.Protocol
 
             if (!IsCorrupted)
             {
-                Header = BigEndian.DecypherUShort(data, 4);
+                Header = BigEndian.DecypherShort(data, 4);
 
                 _body.AddRange(data);
                 _body.RemoveRange(0, 6);
@@ -148,15 +145,60 @@ namespace Sulakore.Habbo.Protocol
             AddToWritten(chunks);
         }
 
-        public byte[] ReadBytes(int length)
+        public void Write(params object[] chunks)
         {
-            int index = Position;
-            byte[] value = ReadBytes(length, ref index);
-            Position = index;
+            AddToWritten(chunks);
+            byte[] constructed = Encode(chunks);
+
+            _body.AddRange(constructed);
+            Reconstruct();
+        }
+        public override int ReadInteger(ref int index)
+        {
+            if (index + 4 > Body.Length)
+                throw new Exception("Not enough data at the current position to read an integer.");
+
+            int value = BigEndian.DecypherInt(Body[index++],
+                Body[index++], Body[index++], Body[index++]);
+
+            AddToRead(value);
             return value;
         }
-        public byte[] ReadBytes(int length, int index) => ReadBytes(length, ref index);
-        public byte[] ReadBytes(int length, ref int index)
+        public override ushort ReadShort(ref int index)
+        {
+            if (index + 2 > Body.Length)
+                throw new Exception("Not enough data at the current position to read a short.");
+
+            ushort value = BigEndian.DecypherShort(
+                Body[index++], Body[index++]);
+
+            AddToRead(value);
+            return value;
+        }
+        public override bool ReadBoolean(ref int index)
+        {
+            if (index + 1 > Body.Length)
+                throw new Exception("Not enough data at the current position to read a boolean.");
+
+            bool value = Body[index++] == 1;
+            AddToRead(value);
+
+            return value;
+        }
+        public override string ReadString(ref int index)
+        {
+            ushort length = ReadShort(ref index);
+
+            if (index + length > Body.Length)
+                throw new Exception("Not enough data at the current position to begin reading a string.");
+
+            string value = Encoding.UTF8.GetString(
+                Body, index, length);
+
+            AddToRead(value);
+            return value;
+        }
+        public override byte[] ReadBytes(int length, ref int index)
         {
             if (length + index > Body.Length)
                 throw new Exception("Not enough data at the current position to read a block of bytes.");
@@ -169,157 +211,28 @@ namespace Sulakore.Habbo.Protocol
             return value;
         }
 
-        public int ReadInteger()
+        public override void Remove<T>(int index)
         {
-            int index = Position;
-            int value = ReadInteger(ref index);
-            Position = index;
-            return value;
-        }
-        public int ReadInteger(int index) => ReadInteger(ref index);
-        public virtual int ReadInteger(ref int index)
-        {
-            if (index + 4 > Body.Length)
-                throw new Exception("Not enough data at the current position to read an integer.");
-
-            int value = BigEndian.DecypherInt(Body[index++],
-                Body[index++], Body[index++], Body[index++]);
-
-            AddToRead(value);
-            return value;
-        }
-
-        public ushort ReadUShort()
-        {
-            int index = Position;
-            ushort value = ReadUShort(ref index);
-            Position = index;
-            return value;
-        }
-        public ushort ReadUShort(int index) => ReadUShort(ref index);
-        public virtual ushort ReadUShort(ref int index)
-        {
-            ushort value = ReadUShortSilently(ref index);
-
-            AddToRead(value);
-            return value;
-        }
-
-        private ushort ReadUShortSilently(int index) => ReadUShortSilently(ref index);
-        protected virtual ushort ReadUShortSilently(ref int index)
-        {
-            if (index + 2 > Body.Length)
-                throw new Exception("Not enough data at the current position to read a short.");
-
-            return BigEndian.DecypherUShort(Body[index++], Body[index++]);
-        }
-
-        public bool ReadBoolean()
-        {
-            int index = Position;
-            bool value = ReadBoolean(ref index);
-            Position = index;
-            return value;
-        }
-        public bool ReadBoolean(int index) => ReadBoolean(ref index);
-        public virtual bool ReadBoolean(ref int index)
-        {
-            if (index + 1 > Body.Length)
-                throw new Exception("Not enough data at the current position to read a boolean.");
-
-            bool value = Body[index++] == 1;
-            AddToRead(value);
-
-            return value;
-        }
-
-        public string ReadString()
-        {
-            int index = Position;
-            string value = ReadString(ref index);
-            Position = index;
-            return value;
-        }
-        public string ReadString(int index) => ReadString(ref index);
-        public virtual string ReadString(ref int index)
-        {
-            ushort length = ReadUShortSilently(ref index);
-
-            if (index + length > Body.Length)
-                throw new Exception("Not enough data at the current position to begin reading a string.");
-
-            string value = Encoding.UTF8.GetString(Body, index, length);
-            index += length;
-            AddToRead(value);
-
-            return value;
-        }
-
-        public void Append(params object[] chunks)
-        {
-            AddToWritten(chunks);
-            byte[] constructed = Encode(chunks);
-
-            _body.AddRange(constructed);
-            Reconstruct();
-        }
-
-        public void ClearChunks()
-        {
-            if (!_beganConstructing)
-                throw new Exception("This method cannot be called on a packet that you did not begin constructing.");
-
-            if (_written.Count < 1)
-                return;
-
-            _body.Clear();
-            _written.Clear();
-
-            Reconstruct();
-        }
-
-        public void Remove<T>()
-        {
-            RemoveAt<T>(Position);
-        }
-        public void RemoveChunk(int rank)
-        {
-            if (!_beganConstructing)
-                throw new Exception("This method cannot be called on a packet that you did not begin constructing.");
-
-            if (rank < 0 || rank >= _written.Count)
-                return;
-
-            _written.RemoveAt(rank);
-
-            _body.Clear();
-            if (_written.Count > 0)
-                _body.AddRange(Encode(_written.ToArray()));
-
-            Reconstruct();
-        }
-        public void RemoveAt<T>(int index)
-        {
-            int chunkLength = 0;
+            int valueSize = 0;
             switch (Type.GetTypeCode(typeof(T)))
             {
-                case TypeCode.Int32: chunkLength = 4; break;
-                case TypeCode.UInt16: chunkLength = 2; break;
-                case TypeCode.Boolean: chunkLength = 1; break;
+                default: return;
+
+                case TypeCode.Int32: valueSize = 4; break;
+                case TypeCode.UInt16: valueSize = 2; break;
+                case TypeCode.Boolean: valueSize = 1; break;
                 case TypeCode.String:
                 {
-                    chunkLength = (2 + ReadUShortSilently(index));
+                    int stringLength = BigEndian.DecypherShort(Body, index);
+                    valueSize = (2 + stringLength);
                     break;
                 }
             }
 
-            if (chunkLength < 1) return;
-            _body.RemoveRange(index, chunkLength);
+            _body.RemoveRange(index, valueSize);
             Reconstruct();
         }
-
-        public bool CanRead<T>() => CanReadAt<T>(Position);
-        public bool CanReadAt<T>(int index)
+        public override bool CanRead<T>(int index)
         {
             int bytesLeft = (Body.Length - index), bytesNeeded = -1;
             if (bytesLeft < 1) return false;
@@ -331,94 +244,31 @@ namespace Sulakore.Habbo.Protocol
                 case TypeCode.Boolean: bytesNeeded = 1; break;
                 case TypeCode.String:
                 {
+                    int stringLength = BigEndian.DecypherShort(Body, index);
+
                     if (bytesLeft > 2)
-                        bytesNeeded = (2 + ReadUShortSilently(index));
+                        bytesNeeded = (2 + stringLength);
                     break;
                 }
             }
             return bytesLeft >= bytesNeeded && bytesNeeded != -1;
         }
-
-        public void Replace<T>(object chunk)
-        {
-            ReplaceAt<T>(Position, chunk);
-        }
-        public void ReplaceChunk(int rank, object chunk)
-        {
-            if (!_beganConstructing)
-                throw new Exception("This method cannot be called on a packet that you did not begin constructing.");
-
-            if (rank < 0 || rank >= _written.Count)
-                return;
-
-            _written[rank] = chunk;
-
-            _body.Clear();
-            _body.AddRange(Encode(_written.ToArray()));
-            Reconstruct();
-        }
-        /// <summary>
-        /// Replaces the chunk at the given position.
-        /// </summary>
-        /// <typeparam name="T">The type of the chunk to replace in the packet.</typeparam>
-        /// <param name="index">The zero-based index that determines the position of the chunk.</param>
-        /// <param name="chunk">The new value to replace the chunk.</param>
-        public void ReplaceAt<T>(int index, object chunk)
+        public override void Replace<T>(int index, object chunk)
         {
             switch (Type.GetTypeCode(typeof(T)))
             {
                 case TypeCode.Int32: _body.RemoveRange(index, 4); break;
                 case TypeCode.UInt16: _body.RemoveRange(index, 2); break;
-
-                case TypeCode.Byte:
                 case TypeCode.Boolean: _body.RemoveAt(index); break;
-
                 case TypeCode.String:
                 {
-                    _body.RemoveRange(index, 2 + ReadUShortSilently(index));
+                    int stringLength = BigEndian.DecypherShort(Body, index);
+                    _body.RemoveRange(index, 2 + stringLength);
                     break;
                 }
             }
+
             _body.InsertRange(index, Encode(chunk));
-            Reconstruct();
-        }
-
-        /// <summary>
-        /// Moves a chunk in the body to the right.
-        /// </summary>
-        /// <typeparam name="T">The type of the chunk to begin pushing.</typeparam>
-        /// <param name="rank">The zero-based value that determines the order in which the chunk was written.</param>
-        /// <param name="jump">The amount of chunks to jump over.</param>
-        public void PushChunk(int rank, int jump)
-        {
-            Move(rank, jump, true);
-        }
-        /// <summary>
-        /// Moves a chunk in the body to the left.
-        /// </summary>
-        /// <typeparam name="T">The type of the chunk to begin pulling.</typeparam>
-        /// <param name="rank">The zero-based value that determines the order in which the chunk was written.</param>
-        /// <param name="jump">The amount of chunks to jump over.</param>
-        public void PullChunk(int rank, int jump)
-        {
-            Move(rank, jump, false);
-        }
-        protected virtual void Move(int rank, int jump, bool isPush)
-        {
-            if (!_beganConstructing)
-                throw new Exception("This method cannot be called on a packet that you did not begin constructing.");
-
-            if (jump < 1) return;
-            int newRank = (isPush ? rank + jump : rank - jump);
-            if (newRank < 0) newRank = 0;
-            if (newRank >= _written.Count) newRank = _written.Count - 1;
-
-            object chunk = _written[rank];
-            _written.Remove(chunk);
-            _written.Insert(newRank, chunk);
-
-            _body.Clear();
-            _body.AddRange(Encode(_written.ToArray()));
             Reconstruct();
         }
 
@@ -446,8 +296,11 @@ namespace Sulakore.Habbo.Protocol
             Buffer.BlockCopy(_body.ToArray(), 0, Body, 0, Body.Length);
         }
 
-        public byte[] ToBytes() => _toBytesCache ?? (_toBytesCache = Construct(Header, Body));
-        public override string ToString() => _toStringCache ?? (_toStringCache = ToString(ToBytes()));
+        public byte[] ToBytes() =>
+            _toBytesCache ?? (_toBytesCache = Construct(Header, Body));
+
+        public override string ToString() =>
+            _toStringCache ?? (_toStringCache = ToString(ToBytes()));
 
         public static byte[] ToBytes(string packet)
         {
