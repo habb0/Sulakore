fa78be3739d3e8366bf3c0ea91145cb65b7a8b30
Arachis <TheOldNutMan@gmail.com>
2015-06-09 10:19:19 -0700

    Added Components namespace.

---
+++ Sulakore/Components/SKoreConstructer.cs
@@ -0,0 +1,119 @@
+﻿/* Copyright
+
+    GitHub(Source): https://GitHub.com/ArachisH/Sulakore
+
+    .NET library for creating Habbo Hotel related desktop applications.
+    Copyright (C) 2015 ArachisH
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    See License.txt in the project root for license information.
+*/
+
+using System.Windows.Forms;
+using System.ComponentModel;
+
+using Sulakore.Habbo.Protocol;
+using System.Collections.Generic;
+
+namespace Sulakore.Components
+{
+    [DesignerCategory("Code")]
+    public class SKoreConstructer : SKoreListView
+    {
+        private const string CHUNK_TIP = "Type: {0}\nValue: {1}\nBlock Length: {2}\nEncoded: {3}";
+
+        private readonly HMessage _packet;
+
+        public int Length => _packet.Length;
+        public ushort Header
+        {
+            get { return _packet.Header; }
+            set { _packet.Header = value; }
+        }
+        public IReadOnlyList<object> ChunksWritten => _packet.ValuesWritten;
+
+        public SKoreConstructer()
+        {
+            _packet = new HMessage(0);
+        }
+
+        public void Append(params object[] chunks)
+        {
+            _packet.Write(chunks);
+            try
+            {
+                BeginUpdate();
+                ListViewItem item = null;
+                byte[] data = new byte[0];
+                SuppressItemSelectedEvent = true;
+                string typeName = string.Empty, value = string.Empty, encoded = string.Empty;
+                foreach (object chunk in chunks)
+                {
+                    value = chunk.ToString();
+
+                    data = HMessage.Encode(chunk);
+                    encoded = HMessage.ToString(data);
+                    typeName = chunk.GetType().Name.Replace("Int32", "Integer");
+
+                    item = FocusAdd(typeName, value, encoded);
+                    item.ToolTipText = string.Format(CHUNK_TIP, typeName, value, data.Length, encoded);
+                }
+                SuppressItemSelectedEvent = false;
+                OnItemSelected(new ListViewItemSelectionChangedEventArgs(item, item.Index, true));
+            }
+            finally { EndUpdate(); }
+        }
+
+        public void ReplaceItem(object chunk)
+        {
+            ListViewItem item = SelectedItems[0];
+            _packet.ReplaceWritten(item.Index, chunk);
+
+            item.SubItems[0].Text = chunk.GetType().Name
+                .Replace("Int32", "Integer");
+
+            byte[] data = HMessage.Encode(chunk);
+            item.SubItems[1].Text = chunk.ToString();
+            item.SubItems[2].Text = HMessage.ToString(data);
+
+            item.ToolTipText = string.Format(CHUNK_TIP,
+                item.SubItems[0].Text, item.SubItems[1].Text, data.Length, item.SubItems[2].Text);
+        }
+        protected override void RemoveItem(ListViewItem listViewItem)
+        {
+            _packet.RemoveWritten(listViewItem.Index);
+            base.RemoveItem(listViewItem);
+        }
+        protected override void MoveItemUp(ListViewItem listViewItem)
+        {
+            _packet.MoveWritten(listViewItem.Index, 1, false);
+            base.MoveItemUp(listViewItem);
+        }
+        protected override void MoveItemDown(ListViewItem listViewItem)
+        {
+            _packet.MoveWritten(listViewItem.Index, 1, true);
+            base.MoveItemDown(listViewItem);
+        }
+
+        public void ClearItems()
+        {
+            Items.Clear();
+            _packet.ClearWritten();
+        }
+        public byte[] GetBytes() => _packet.ToBytes();
+        public string GetString() => _packet.ToString();
+    }
+}
---
+++ Sulakore/Components/SKoreScheduler.cs
@@ -0,0 +1,216 @@
+﻿/* Copyright
+
+    GitHub(Source): https://GitHub.com/ArachisH/Sulakore
+
+    .NET library for creating Habbo Hotel related desktop applications.
+    Copyright (C) 2015 ArachisH
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    See License.txt in the project root for license information.
+*/
+
+using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+using Sulakore.Habbo.Protocol;
+
+namespace Sulakore.Components
+{
+    [DesignerCategory("Code")]
+    public class SKoreScheduler : SKoreListView
+    {
+        public event EventHandler<HScheduleTriggeredEventArgs> ScheduleTriggered;
+
+        private readonly Dictionary<HSchedule, ListViewItem> _items;
+        private readonly Dictionary<ListViewItem, HSchedule> _schedules;
+        private readonly Dictionary<ListViewItem, string> _descriptions;
+
+        private const string RUNNING = "Running", STOPPED = "Stopped";
+
+        [Browsable(false)]
+        [DefaultValue(true)]
+        public bool AutoStart { get; set; }
+
+        public int Running => _items.Keys.Count(x => x.IsRunning);
+
+        public SKoreScheduler()
+        {
+            _items = new Dictionary<HSchedule, ListViewItem>();
+            _descriptions = new Dictionary<ListViewItem, string>();
+            _schedules = new Dictionary<ListViewItem, HSchedule>();
+
+            AutoStart = true;
+            CheckBoxes = true;
+        }
+
+        public void ClearItems()
+        {
+            foreach (ListViewItem item in Items)
+                RemoveItem(item);
+        }
+        protected override void RemoveItem(ListViewItem listViewItem)
+        {
+            if (_schedules.ContainsKey(listViewItem))
+            {
+                HSchedule schedule = _schedules[listViewItem];
+                _items.Remove(schedule);
+
+                schedule.Dispose();
+                _schedules.Remove(listViewItem);
+
+                _descriptions.Remove(listViewItem);
+            }
+
+            base.RemoveItem(listViewItem);
+        }
+        public void AddSchedule(HMessage packet, int burst, int interval, string description)
+        {
+            if (packet.IsCorrupted)
+                throw new Exception("Provided packet is either corrupted, or has Destination set to 'Unknown'.");
+
+            var item = new ListViewItem(new[] { packet.ToString(),
+                packet.Destination.ToString(), burst.ToString(), interval.ToString(), AutoStart ? RUNNING : STOPPED });
+
+            var schedule = new HSchedule(packet, interval, burst);
+            schedule.ScheduleTriggered += OnScheduleTriggered;
+
+            _items.Add(schedule, item);
+            _schedules.Add(item, schedule);
+            _descriptions.Add(item, description);
+
+            item.Checked = AutoStart;
+            item.ToolTipText = description;
+
+            FocusAdd(item);
+        }
+
+        public void StopAllSchedules()
+        {
+            foreach (ListViewItem item in _schedules.Keys)
+                item.Checked = false;
+        }
+        public void StartAllSchedules()
+        {
+            foreach (ListViewItem item in _schedules.Keys)
+                item.Checked = true;
+        }
+
+        public int GetItemBurst() => SelectedItems.Count > 0 ?
+                _schedules[SelectedItems[0]].Burst : 0;
+        public void SetItemBurst(int burst)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            _schedules[item].Burst = burst;
+            item.SubItems[2].Text = burst.ToString();
+        }
+
+        public int GetItemInterval() => SelectedItems.Count > 0 ?
+                _schedules[SelectedItems[0]].Interval : 0;
+        public void SetItemInterval(int interval)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            _schedules[item].Interval = interval;
+            item.SubItems[2].Text = interval.ToString();
+        }
+
+        public string GetItemDescription() => SelectedItems.Count > 0 ?
+                _descriptions[SelectedItems[0]] : string.Empty;
+        public void SetItemDescription(string description)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            item.ToolTipText = description;
+            _descriptions[item] = description;
+        }
+
+        public HMessage GetItemPacket() => SelectedItems.Count > 0 ?
+                _schedules[SelectedItems[0]].Packet : null;
+        public void SetItemPacket(HMessage packet)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            _schedules[item].Packet = packet;
+            item.SubItems[0].Text = packet.ToString();
+            item.SubItems[1].Text = packet.Destination.ToString();
+        }
+
+        public HDestination GetItemDestination() => SelectedItems.Count > 0 ?
+                _schedules[SelectedItems[0]].Packet.Destination : HDestination.Client;
+        public void SetItemDestination(HDestination destination)
+        {
+            if (SelectedItems.Count < 1) return;
+
+            ListViewItem item = SelectedItems[0];
+            _schedules[item].Packet.Destination = destination;
+            item.SubItems[1].Text = destination.ToString();
+        }
+
+        protected override void OnItemChecked(ItemCheckedEventArgs e)
+        {
+            if (!_schedules.ContainsKey(e.Item)) return;
+
+            HSchedule schedule = _schedules[e.Item];
+            e.Item.SubItems[4].Text = e.Item.Checked ? RUNNING : STOPPED;
+
+            if (e.Item.Checked)
+                schedule.Start();
+            else if (schedule.IsRunning)
+                schedule.Stop();
+
+            base.OnItemChecked(e);
+        }
+        protected virtual void OnScheduleTriggered(object sender, HScheduleTriggeredEventArgs e)
+        {
+            EventHandler<HScheduleTriggeredEventArgs> handler = ScheduleTriggered;
+            if (handler != null)
+            {
+                try { handler(sender, e); }
+                catch { e.Cancel = true; }
+                finally
+                {
+                    if (e.Cancel)
+                    {
+                        Invoke(new MethodInvoker(() =>
+                        {
+                            ListViewItem item = _items[(HSchedule)sender];
+                            item.SubItems[4].Text = STOPPED;
+                            item.Checked = false;
+                        }));
+                    }
+                }
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                foreach (HSchedule schedule in _schedules.Values)
+                    schedule.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
