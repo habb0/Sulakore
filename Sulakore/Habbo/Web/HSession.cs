bb2648847ab40c1b2f6030ed5270bfbe0ff75ae1
Arachis <TheOldNutMan@gmail.com>
2015-06-12 14:31:17 -0700

    Updated documentation. Finished connection, thingy. Added GameData property. (HGameData) Added SsoTicket property. Added IsReading property to decide whether to read incoming data if connected.

--- Sulakore/Habbo/Web/HSession.cs
+++ Sulakore/Habbo/Web/HSession.cs
@@ -26,60 +26,90 @@ using System;
 using System.IO;
 using System.Net;
 using System.Text;
-using System.Net.Sockets;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Sulakore.Communication;
 using Sulakore.Habbo.Protocol;
-using Sulakore.Habbo.Protocol.Encoders;
-using Sulakore.Habbo.Protocol.Encryption;
 
 namespace Sulakore.Habbo.Web
 {
-    // TODO: Finish
-
     public class HSession : IDisposable
     {
         private readonly Uri _hotelUri;
-        private readonly object _sendToServerLock;
+        private readonly object _disconnectLock;
 
+        /// <summary>
+        /// Occurs when the connection to the remote <see cref="HNode"/> has been established.
+        /// </summary>
         public event EventHandler<EventArgs> Connected;
+        /// <summary>
+        /// Raises the <see cref="Connected"/> event.
+        /// </summary>
+        /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
         protected virtual void OnConnected(EventArgs e)
         {
             EventHandler<EventArgs> handler = Connected;
             if (handler != null) handler(this, e);
         }
-
+        /// <summary>
+        /// Occurs when either client/server have been disconnected, or when <see cref="Disconnect"/> has been called if the <see cref="HConnection"/> is currently connected.
+        /// </summary>
         public event EventHandler<EventArgs> Disconnected;
+        /// <summary>
+        /// Raises the <see cref="Disconnected"/> event.
+        /// </summary>
+        /// <param name="e">An <see cref="EventArgs"/> that contains the event data.</param>
         protected virtual void OnDisconnected(EventArgs e)
         {
             EventHandler<EventArgs> handler = Disconnected;
             if (handler != null) handler(this, e);
         }
-
-        public event EventHandler<InterceptedEventArgs> DataToClient;
-        protected virtual void OnDataToClient(InterceptedEventArgs e)
+        /// <summary>
+        /// Occurs when incoming data from the server has been intercepted.
+        /// </summary>
+        public event EventHandler<InterceptedEventArgs> DataIncoming;
+        /// <summary>
+        /// Raises the <see cref="DataIncoming"/> event.
+        /// </summary>
+        /// <param name="e">An <see cref="InterceptedEventArgs"/> that contains the event data.</param>
+        /// <returns></returns>
+        protected virtual void OnDataIncoming(InterceptedEventArgs e)
         {
-            EventHandler<InterceptedEventArgs> handler = DataToClient;
+            EventHandler<InterceptedEventArgs> handler = DataIncoming;
             if (handler != null) handler(this, e);
         }
 
         public HHotel Hotel { get; }
         public string Email { get; }
         public string Password { get; }
+        public HTriggers Triggers { get; }
         public CookieContainer Cookies { get; }
 
-        public int TotalIncoming { get; private set; }
-
-        public int Port { get; }
-        public string Host { get; }
-        public string[] Addresses { get; }
-
-        private Socket Remote { get; set; }
         public HUser User { get; private set; }
+        public HNode Remote { get; private set; }
+        public string SsoTicket { get; private set; }
+        public int TotalIncoming { get; private set; }
+        public HGameData GameData { get; private set; }
 
         public bool IsDisposed { get; private set; }
+        public bool IsConnected { get; private set; }
         public bool IsAuthenticated { get; private set; }
+
+        private bool _isReading;
+        public bool IsReading
+        {
+            get { return _isReading; }
+            set
+            {
+                if (_isReading == value)
+                    return;
+
+                if (_isReading = value)
+                    ReadIncomingAsync();
+            }
+        }
 
         static HSession()
         {
@@ -87,175 +117,136 @@ namespace Sulakore.Habbo.Web
             ServicePointManager.ServerCertificateValidationCallback =
                 (sender, certificate, chain, sslPolicyErrors) => true;
         }
-        public HSession(string email,
-            string password, HHotel hotel)
+        public HSession(string email, string password, HHotel hotel)
         {
             Email = email;
             Hotel = hotel;
             Password = password;
+            Triggers = new HTriggers(false);
             Cookies = new CookieContainer();
 
-            _sendToServerLock = new object();
+            _disconnectLock = new object();
             _hotelUri = new Uri(Hotel.ToUrl());
-
-            Remote = new Socket(AddressFamily.InterNetwork,
-                SocketType.Stream, ProtocolType.Tcp);
         }
 
-        public async Task<bool> LoginAsync()
+        public void Disconnect()
         {
-            IsAuthenticated = false;
-            try
+            if (IsConnected && Monitor.TryEnter(_disconnectLock))
             {
-                Cookies.SetCookies(_hotelUri,
-                    await SKore.GetIPCookieAsync(Hotel).ConfigureAwait(false));
-
-                byte[] postData = Encoding.UTF8
-                    .GetBytes($"{{\"email\":\"{Email}\",\"password\":\"{Password}\"}}");
-
-                var loginRequest = (HttpWebRequest)WebRequest
-                    .Create($"{_hotelUri.OriginalString}/api/public/authentication/login");
-
-                loginRequest.ContentType = "application/json;charset=UTF-8";
-                loginRequest.CookieContainer = Cookies;
-                loginRequest.AllowAutoRedirect = false;
-                loginRequest.Method = "POST";
-                loginRequest.Proxy = null;
-
-                using (Stream requestStream = await loginRequest
-                    .GetRequestStreamAsync().ConfigureAwait(false))
+                try
                 {
-                    await requestStream.WriteAsync(postData, 0, postData.Length)
-                        .ConfigureAwait(false);
+                    IsReading = false;
+                    Remote.Dispose();
+
+                    IsConnected = false;
+                    OnDisconnected(EventArgs.Empty);
+
+                    TotalIncoming = 0;
                 }
-
-                using (WebResponse loginResponse = await loginRequest.GetResponseAsync()
-                    .ConfigureAwait(false))
+                finally
                 {
-                    using (Stream loginStream = loginResponse.GetResponseStream())
-                    using (var loginReader = new StreamReader(loginStream))
-                    {
-                        string body = await loginReader.ReadToEndAsync()
-                            .ConfigureAwait(false);
-
-                        User = HUser.Create(body);
-
-                        string cookies = loginResponse
-                            .Headers[HttpResponseHeader.SetCookie];
-
-                        Cookies.SetCookies(_hotelUri, cookies);
-
-                        return (IsAuthenticated = ((HttpWebResponse)loginResponse)
-                            .StatusCode == HttpStatusCode.OK);
-                    }
+                    Monitor.Exit(_disconnectLock);
                 }
             }
-            catch (WebException) { return false; }
         }
-
-        public override string ToString()
-            => $"{Email}:{Password}:{Hotel.ToDomain()}";
-
-        public bool IsConnected { get; private set; }
-
-        /// <summary>
-        /// Gets or sets the <see cref="Rc4"/> for encrypting outgoing data.
-        /// </summary>
-        public Rc4 LocaEncrypter { get; set; }
-        /// <summary>
-        /// Gets or sets the <see cref="Rc4"/> for decrypting incoming data.
-        /// </summary>
-        public Rc4 RemoteDecrypter { get; set; }
-
         public async Task ConnectAsync()
         {
-            Remote = new Socket(AddressFamily.InterNetwork,
-                SocketType.Stream, ProtocolType.Tcp);
-            //  await Remote.ConnectTaskAsync(Host, Port).ConfigureAwait(false);
-
-            ReadIncomingAsync();
+            Remote = await HNode.ConnectAsync(GameData.Host, GameData.Port)
+                .ConfigureAwait(false);
 
             IsConnected = true;
             OnConnected(EventArgs.Empty);
+
+            IsReading = true;
+        }
+        public async Task<bool> LoginAsync()
+        {
+            IsAuthenticated = false;
+            Cookies.SetCookies(_hotelUri, await SKore.GetIPCookieAsync(Hotel).ConfigureAwait(false));
+            byte[] postData = Encoding.UTF8.GetBytes($"{{\"email\":\"{Email}\",\"password\":\"{Password}\"}}");
+
+            var loginRequest = (HttpWebRequest)WebRequest.Create($"{_hotelUri.OriginalString}/api/public/authentication/login");
+            loginRequest.ContentType = "application/json;charset=UTF-8";
+            loginRequest.CookieContainer = Cookies;
+            loginRequest.AllowAutoRedirect = false;
+            loginRequest.Method = "POST";
+            loginRequest.Proxy = null;
+
+            using (Stream requestStream = await loginRequest.GetRequestStreamAsync().ConfigureAwait(false))
+                await requestStream.WriteAsync(postData, 0, postData.Length).ConfigureAwait(false);
+
+            using (WebResponse loginResponse = await loginRequest.GetResponseAsync().ConfigureAwait(false))
+            using (Stream loginStream = loginResponse.GetResponseStream())
+            using (var loginReader = new StreamReader(loginStream))
+            {
+                string body = await loginReader.ReadToEndAsync().ConfigureAwait(false);
+
+                User = HUser.Create(body);
+                Cookies.SetCookies(_hotelUri, loginResponse.Headers["Set-Cookie"]);
+                IsAuthenticated = ((HttpWebResponse)loginResponse).StatusCode == HttpStatusCode.OK;
+
+                if (IsAuthenticated)
+                    await ExtractGameDataAsync().ConfigureAwait(false);
+
+                return IsAuthenticated;
+            }
+        }
+
+        public Task<int> SendToServerAsync(byte[] data)
+        {
+            return Remote.SendAsync(data);
+        }
+        public Task<int> SendToServerAsync(ushort header, params object[] chunks)
+        {
+            return Remote.SendAsync(HMessage.Construct(header, chunks));
         }
 
         private async Task ReadIncomingAsync()
         {
-            byte[] lengthBlock = new byte[4];
-            //  await Remote.ReceiveTaskAsync(lengthBlock, 0, 4)
-            //      .ConfigureAwait(false);
-
-            RemoteDecrypter?.Parse(lengthBlock);
-            int bodyLength = BigEndian.DecypherInt(lengthBlock);
-
-            int bytesRead = 0;
-            int totalBytesRead = 0;
-            byte[] body = new byte[bodyLength];
-            while (totalBytesRead != body.Length)
-            {
-                byte[] block = new byte[bodyLength - totalBytesRead];
-
-                //  bytesRead = await Remote.ReceiveTaskAsync(block, 0, block.Length)
-                //      .ConfigureAwait(false);
-
-                Buffer.BlockCopy(block, 0, body, totalBytesRead, bytesRead);
-                totalBytesRead += bytesRead;
-            }
-            RemoteDecrypter?.Parse(body);
-
-            byte[] packet = new byte[4 + body.Length];
-            Buffer.BlockCopy(lengthBlock, 0, packet, 0, 4);
-            Buffer.BlockCopy(body, 0, packet, 4, body.Length);
-
+            byte[] packet = await Remote.ReceiveAsync().ConfigureAwait(false);
             HandleIncoming(packet, ++TotalIncoming);
         }
+        private async Task ExtractGameDataAsync()
+        {
+            var clientUrlRequest = (HttpWebRequest)WebRequest.Create($"{_hotelUri.OriginalString}/api/client/clienturl");
+            clientUrlRequest.ContentType = "application/json;charset=UTF-8";
+            clientUrlRequest.CookieContainer = Cookies;
+            clientUrlRequest.AllowAutoRedirect = false;
+            clientUrlRequest.Method = "GET";
+            clientUrlRequest.Proxy = null;
 
+            using (WebResponse clientUrlResponse = await clientUrlRequest.GetResponseAsync().ConfigureAwait(false))
+            using (Stream clientUrlStream = clientUrlResponse.GetResponseStream())
+            using (var clientUrlReader = new StreamReader(clientUrlStream))
+            {
+                string clientUrl = await clientUrlReader.ReadToEndAsync();
+                clientUrl = clientUrl.GetChild("{\"clienturl\":\"", '"');
+
+                using (var client = new WebClient())
+                {
+                    client.Proxy = null;
+                    client.Headers["Cookie"] =
+                        clientUrlRequest.Headers["Cookie"];
+
+                    SsoTicket = clientUrl.Split('/').Last();
+                    string clientBody = await client.DownloadStringTaskAsync(clientUrl);
+                    GameData = new HGameData(clientBody);
+                }
+            }
+        }
         private void HandleIncoming(byte[] data, int count)
         {
             var args = new InterceptedEventArgs(ReadIncomingAsync, count, data, HDestination.Client);
-            //Triggers.HandleIncoming(args);
+            Triggers.HandleIncoming(args);
 
-            //if (!args.Cancel && !BlockedIncoming.Contains(args.Packet.Header))
-            //{
-            //    if (IncomingIntercepted != null)
-            //        IncomingIntercepted(this, args);
+            OnDataIncoming(args);
 
-            //    if (!args.Cancel)
-            //        SendToClient(args.Packet.ToBytes());
-            //}
-
-            if (!args.WasContinued)
+            if (!args.WasContinued && IsReading)
                 ReadIncomingAsync();
         }
-        
-        public void Disconnect()
-        {
-            if (Remote.Connected)
-            {
-                Remote.Shutdown(SocketShutdown.Both);
-                Remote.Close();
-            }
 
-            //_inStep = 0;
-            //_inCache = null;
-            //_shouldReceive = false;
-
-            //RemoteDecypter = null;
-            LocaEncrypter = null;
-
-            OnDisconnected(EventArgs.Empty);
-        }
-
-        public int SendToServer(byte[] data)
-        {
-            lock (_sendToServerLock)
-            {
-                data = LocaEncrypter?.SafeParse(data) ?? data;
-                return Remote.Send(data);
-            }
-        }
-        public int SendToServer(ushort header, params object[] chunks) =>
-            SendToServer(HMessage.Construct(header, chunks));
+        public override string ToString()
+            => $"{Email}:{Password}:{Hotel.ToDomain()}";
 
         public void Dispose()
         {
@@ -267,15 +258,11 @@ namespace Sulakore.Habbo.Web
             {
                 if (disposing)
                 {
-                    User = null;
-                    // _inCache = null;
-                    //_inBuffer = null;
-
+                    Triggers.Dispose();
                     SKore.Unsubscribe(ref Connected);
                     SKore.Unsubscribe(ref Disconnected);
-                    SKore.Unsubscribe(ref DataToClient);
-
-                    Remote.Dispose();
+                    SKore.Unsubscribe(ref DataIncoming);
+                    Disconnect();
                 }
                 IsDisposed = true;
             }
