8915afffa0727bdc5b02adc64f14b5b57111bde6
Arachis <TheOldNutMan@gmail.com>
2015-06-12 14:28:42 -0700

    Added interface to represent an in-game object with a room index identifier.

---
+++ Sulakore/Habbo/IHEntity.cs
@@ -0,0 +1,37 @@
+﻿/* Copyright
+
+    GitHub(Source): https://GitHub.com/ArachisH/Sulakore
+
+    .NET library for creating Habbo Hotel related desktop applications.
+    Copyright (C) 2015 ArachisH
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    See License.txt in the project root for license information.
+*/
+
+namespace Sulakore.Habbo
+{
+    /// <summary>
+    /// Represents an in-game object that provides a unique identifier relative to the room.
+    /// </summary>
+    public interface IHEntity
+    {
+        /// <summary>
+        /// Gets the room index value of the <see cref="IHEntity"/>.
+        /// </summary>
+        int Index { get; }
+    }
+}
